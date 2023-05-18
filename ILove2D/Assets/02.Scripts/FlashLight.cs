edb00aad9189043d5d1c6944b676088c24768d6d
ahslxjvhapt12 <likjhee1@gmail.com>
2023-05-18 11:16:59 +0900

    삼각함수 덧셈

--- ILove2D/Assets/02.Scripts/FlashLight.cs
+++ ILove2D/Assets/02.Scripts/FlashLight.cs
@@ -5,13 +5,33 @@ using UnityEngine.Rendering.Universal;
 
 public class FlashLight : MonoBehaviour
 {
-    float t;
+    float t1;
+    float t2;
+    float t3;
+
     [SerializeField] private Light2D _light;
+
+    [Header("ÁÖ±â")]
+    [SerializeField] float speed1;
+    [SerializeField] float speed2;
+    [SerializeField] float speed3;
+    
+    [Header("Æø")]
+    [SerializeField] float p1;
+    [SerializeField] float p2;
+    [SerializeField] float p3;
 
     private void Update()
     {
-        t += Time.deltaTime;
+        t1 += Time.deltaTime * speed1;
+        t2 += Time.deltaTime * speed2;
+        t3 += Time.deltaTime * speed3;
 
-        _light.intensity = (Mathf.Cos(t) + 1) * 3f;
+        float f1 = Mathf.Cos(t1) * p1;
+        float f2 = Mathf.Cos(t2) * p2;
+        float f3 = Mathf.Cos(t3) * p3;
+
+        float sum = ((f1 + f2 + f3) / (p1 + p2 + p3) * 2) + 2;
+        _light.intensity = sum;
     }
 }
--- ILove2D/Assets/02.Scripts/KassadinR.cs
+++ ILove2D/Assets/02.Scripts/KassadinR.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KassadinR : MonoBehaviour
 {
@@ -10,10 +11,15 @@ public class KassadinR : MonoBehaviour
 
     [SerializeField] float mana;
     [SerializeField] float cost;
-    [SerializeField] float power;
+    [SerializeField] float distance;
+    [SerializeField] float currentCool;
+    [SerializeField] float cool;
 
     [SerializeField] TextMeshProUGUI manaText;
     [SerializeField] TextMeshProUGUI costText;
+
+    [SerializeField] Slider slider;
+    [SerializeField] TextMeshProUGUI coolText;
 
     private void Awake()
     {
@@ -23,14 +29,28 @@ public class KassadinR : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1) && mana >= cost)
+        slider.value = currentCool / cool;
+        coolText.text = $"{currentCool : 0.0}s";
+        if (currentCool <= 0)
         {
-            mana -= cost;
-            cost *= 2;
-            manaText.text = $"mana : {mana}";
-            costText.text = $"cost : {cost}";
-            Vector3 clickPosition = mainCam.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, Camera.main.transform.position.y));
-            _rigid.AddForce((clickPosition - transform.position).normalized * power, ForceMode2D.Impulse);
+            if (Input.GetMouseButtonDown(1) && mana >= cost)
+            {
+                mana -= cost;
+                cost *= 2;
+                manaText.text = $"mana : {mana}";
+                costText.text = $"cost : {cost}";
+                Vector3 clickPosition = mainCam.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, Camera.main.transform.position.y));
+                //_rigid.AddForce((clickPosition - transform.position).normalized * power, ForceMode2D.Impulse);
+                transform.position = new Vector3(
+                    (transform.position + (clickPosition - transform.position).normalized * distance).x,
+                    (transform.position + (clickPosition - transform.position).normalized * distance).y,
+                    0);
+                currentCool = cool;
+            }
+        }
+        else
+        {
+            currentCool -= Time.deltaTime;
         }
     }
 }
