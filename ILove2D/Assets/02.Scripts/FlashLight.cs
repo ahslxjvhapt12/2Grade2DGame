a4c10caceb6d433c644e0b3f4852020fcbdefc52
ahslxjvhapt12 <likjhee1@gmail.com>
2023-05-22 16:29:23 +0900

    Math
    삼각함수 움직임

--- ILove2D/Assets/02.Scripts/FlashLight.cs
+++ ILove2D/Assets/02.Scripts/FlashLight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,17 @@ public class FlashLight : MonoBehaviour
     float t1;
     float t2;
     float t3;
+    [Header("경과 시간")]
+    [SerializeField] private float t = 0;
 
+    [SerializeField] private GameObject bomb;
     [SerializeField] private Light2D _light;
 
     [Header("주기")]
     [SerializeField] float speed1;
     [SerializeField] float speed2;
     [SerializeField] float speed3;
-    
+
     [Header("폭")]
     [SerializeField] float p1;
     [SerializeField] float p2;
@@ -31,7 +35,9 @@ public class FlashLight : MonoBehaviour
         float f2 = Mathf.Cos(t2) * p2;
         float f3 = Mathf.Cos(t3) * p3;
 
-        float sum = ((f1 + f2 + f3) / (p1 + p2 + p3) * 2) + 2;
+        float sum = ((f1 + f2 + f3) / (p1 + p2 + p3)) * 0.5f + 0.5f;
         _light.intensity = sum;
+        t += Time.deltaTime;
+        bomb.transform.position = new Vector2(t, sum * 3);
     }
 }
